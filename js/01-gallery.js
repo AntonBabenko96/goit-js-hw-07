ddb41e9be924642400d467662d7919eba69b88f3
AntonBabenko96 <ababenko159@gmail.com>
2023-01-09 16:28:48 +0200

    qq

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener("click", onGalleryClick)
+function createGalleryMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+        
+})
+.join(" ")
+}
+function onGalleryClick(e) {
+    e.preventDefault()
 
-console.log(galleryItems);
+    if (!e.target.classList.contains("gallery__image")) {
+        return
+    }
+   
+    const linkLargeImg = e.target.dataset.source;
+   const instance = basicLightbox.create(`
+    <img src="${linkLargeImg}" width="800" height="600">
+`)
+
+instance.show()
+
+}
+
